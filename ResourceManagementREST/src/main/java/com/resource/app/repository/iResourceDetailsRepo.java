81374bff2b68ffae466c0e35c13f323fbb5ac75e
sahadan <sahadan@gmail.com>
2020-04-01 20:32:33 +0530

    First Commit

---
+++ ResourceManagementREST/src/main/java/com/resource/app/repository/iResourceDetailsRepo.java
@@ -0,0 +1,33 @@
+package com.resource.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import com.resource.app.model.ResourceDetails;
+
+public interface iResourceDetailsRepo extends JpaRepositoryImplementation<ResourceDetails, Long> {
+
+	@Query("from ResourceDetails order by resourceId desc")
+	List<ResourceDetails> listAllResource();
+	
+	@Query("from ResourceDetails where isActive='Y'")
+	List<ResourceDetails> listResourcedetails();
+
+	@Query("from ResourceDetails WHERE resource.resourceType=?1")
+	List<ResourceDetails> findByResource(String resourceName);
+
+	@Modifying
+	@Query("update ResourceDetails set isActive=?1 where resourceId=?2")
+	void save(Character isActive,Long resourceId);
+	
+	@Modifying
+	@Query("update ResourceDetails set isBooked=?1 where resourceId=?2")
+	void saveIsBooked(Character isBooked,Long resourceId);
+
+
+	@Modifying
+	@Query("update ResourceDetails set isAccepted=?1 where resourceId=?2")
+	void saveIsAccepted(Character isAccepted,Long resourceId);
+}
---
+++ ResourceManagementREST/src/main/java/com/resource/app/service/iResourceDetailsService.java
@@ -0,0 +1,30 @@
+package com.resource.app.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.resource.app.model.ResourceDetails;
+
+public interface iResourceDetailsService {
+	public ResourceDetails addResource(ResourceDetails resourceDetails);
+
+	public ResourceDetails updateResource(ResourceDetails resourceDetails);
+
+	public List<ResourceDetails> listAllResource();
+
+	public ResourceDetails findResourceById(Long resourceId);
+
+	public List<ResourceDetails> findResourcesByName(String resourceName);
+
+	public void updateIsactive(Character isActive, Long resourceId);
+	
+	public void updateIsBooked(Character isBooked, Long resourceId);
+	
+	public void updateIsAccepted(Character isAccepted, Long resourceId);
+	
+	public List<ResourceDetails> listResourceDetails();
+	
+
+
+}
